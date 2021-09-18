aa0359bfc0b1d1f59c64675257b1768dabb3b039
DhrumilP <dhrumil172@gmail.com>
2021-09-18 18:12:15 -0400

    Refactored dir

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import React from 'react'
+import Testing from './components/Testing';
 
 function App() {
+ 
   return (
-   <h1>Hello World</h1>
+   < Testing/>
   );
 }
 
---
+++ src/components/Testing.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react'
+
+function Testing() {
+    useEffect(() => {
+        console.log('hello world')
+      })
+      
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Testing
