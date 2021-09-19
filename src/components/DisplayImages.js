3848b86bedf625ec9d1c2c78dec482c99d40d0f3
Madhu Sivapragasam <madhuran.siva@gmail.com>
2021-09-18 22:08:20 -0400

    Adding initial delete image logic

--- src/components/DisplayImages.js
+++ src/components/DisplayImages.js
@@ -1,35 +1,36 @@
-import React, {useEffect, useState} from 'react'
-import { collection, getDocs} from "firebase/firestore";
-import {db} from "../firebase.js"
-
+import React, { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase.js";
 
 function DisplayImages() {
-  const [images, setImages] = useState([])
-    useEffect (()=>{
-        async function getData() {
-          const querySnapshot = await getDocs(collection(db, "images"));
-          querySnapshot.forEach((doc) => {
-            // doc.data() is never undefined for query doc snapshots
-            console.log(doc.id, " => ", doc.data());
-            setImages(images => [...images, doc.data().tag]);
-            console.log("image", images)
-          });
-        }
-        getData()
-      },[])
-    return (
-      <div>
-        {images.map((image)=>{
-          return (
-            <div>
-              <div>"hi"</div>
-              <img src={image}/>
-            </div>
-          );
-        })}
-      </div>
-
-    )
+  const [images, setImages] = useState([]);
+  useEffect(() => {
+    async function getData() {
+      const querySnapshot = await getDocs(collection(db, "images"));
+      querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id, " => ", doc.data());
+        setImages((images) => [...images, { tag: doc.data().tag, id: doc.id }]);
+      });
+    }
+    getData();
+  }, []);
+  return (
+    <div>
+      {images.map((image) => {
+        const deleteImage = (image) => {
+          console.log("id", image.id);
+          console.log("image", images);
+        };
+        return (
+          <div key={image.id}>
+            <button onClick={() => deleteImage(image)}>delete</button>
+            <img src={image.tag} />
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
-export default DisplayImages
+export default DisplayImages;
